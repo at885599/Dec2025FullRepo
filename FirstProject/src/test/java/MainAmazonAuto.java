89d3a65f5b48ecf81bc37cfe2ca3c656e1f99901
Ashish Tripathi <at885599@gmail.com>
2025-12-29 01:47:15 +0530

    printing once amazon launches

--- FirstProject/src/test/java/MainAmazonAuto.java
+++ FirstProject/src/test/java/MainAmazonAuto.java
@@ -9,6 +9,7 @@ public class MainAmazonAuto extends BaseTest{
 	public void Amazon() {
 		//driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		driver.get("https://amazon.in");
+		System.out.println("amazon opened");
 		amazonHomepage ahp=new amazonHomepage(driver);
 		ahp.enterProductAndSearch("Asics Japan S mens");
 		ProductResults pr=new ProductResults(driver);
